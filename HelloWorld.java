3a932850d67559bb0f14b64bf0d5bba70afa0389
Emily Prud'hommeaux <emilypx@Chica.local>
2018-08-26 20:42:37 -0400

    Added goodbye

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,11 @@
 // This is how you write a comment.
+// This is another comment.
 
 public class HelloWorld {
 
   public static void main (String[] args) {
     System.out.println("Hello, World!");
+    System.out.println("Goodbye, World!");
   }
 
 }
